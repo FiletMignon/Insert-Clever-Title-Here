04b1effdfc885bac502746c30606d10c4aed5321
wiful <willfuller95@gmail.com>
2012-06-09 17:05:54 -0700

    optimization of cube

--- src/kontrol/main/Cube.java
+++ src/kontrol/main/Cube.java
@@ -1,25 +1,24 @@
 package kontrol.main;
 
 public class Cube extends Entity{
-	private Velocity velocity;
 	public Cube(String texture, Position pos) {
 		super(texture, new BoundingBox(1,1,1), pos);
-		velocity = new Velocity();
+		vel = new Velocity();
 	}
 	public void setVelocity(float x, float y, float z){
-		velocity = new Velocity(x, y, z);
+		vel = new Velocity(x, y, z);
 	}
 	public void act(Environment enviro){
 		int entityAmount = enviro.getEntityAmount();
 		for(int i = 0; i < entityAmount; i++){
 			Entity indexedEnt = enviro.getEntity(i);
 			if(isCollidedWith(indexedEnt)){
-				velocity.setVelocity(0, 0, 0);
+				vel.setVelocity(0, 0, 0);
 			}
 			else{
-				pos.x(velocity.getXVelocity());
-				pos.y(velocity.getYVelocity());
-				pos.z(velocity.getZVelocity());
+				pos.x(vel.getXVelocity());
+				pos.y(vel.getYVelocity());
+				pos.z(vel.getZVelocity());
 			}
 		}
 	}
