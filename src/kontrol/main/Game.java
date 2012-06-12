2d63344472b91495f1ea08d208abe6db62f790ad
wiful <willfuller95@gmail.com>
2012-06-12 14:47:08 -0700

    creation of a physics demo

--- src/kontrol/main/Game.java
+++ src/kontrol/main/Game.java
@@ -2,7 +2,7 @@ package kontrol.main;
 
 public class Game {
 	public Game(){
-		new Screen(800, 600, "GravCube");
+		new PhysicsDemo(800, 600, "GravCube");
 	}
 	
 	public static void main(String[] args){
