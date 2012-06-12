2069fd7d674ce013c68e417bc7d0dbde3a324aaf
Kontrol Ke'al <aaronbekkerdulmage@gmail.com>
2012-06-11 23:24:18 -0700

    Changed the Bullet a bit

--- src/kontrol/main/entities/Bullet.java
+++ src/kontrol/main/entities/Bullet.java
@@ -11,22 +11,20 @@ public class Bullet extends Entity{
 	private Position shootTo;
 	private Position shootFrom;
 	
-	private Position force;
 	public Bullet(Position shootFrom, Position shootTo){
 		super(null, new BoundingBox(0.05f, 0.05f, 0.05f), shootFrom);
 		this.shootFrom = shootFrom;
-		this.shootFrom.backward(1.0f);
 		this.shootTo = shootTo;
-		force = shootTo;
-		setForce(force);
+		this.mass = 5;
+		
 	}
 	public void render(){
 		float width = boundingBox.getWidth()/2;
 		float height = boundingBox.getHeight()/2;
 		float depth = boundingBox.getDepth()/2;
 		GL11.glTranslatef(pos.x(), pos.y(), pos.z());
-		GL11.glColor3f(0.0f, 0.0f, 1.0f);
 	    GL11.glBegin(GL11.GL_QUADS);                        // Draw A Quad
+			GL11.glColor3f(1.0f, 0.0f, 0.0f);
 		    
 		    GL11.glVertex3f( width, height,-depth);         // Top Right Of The Quad (Top)
 		    GL11.glVertex3f(-width, height,-depth);         // Top Left Of The Quad (Top)
@@ -57,9 +55,9 @@ public class Bullet extends Entity{
 		    GL11.glVertex3f( width, height, depth);         // Top Left Of The Quad (Right)
 		    GL11.glVertex3f( width,-height, depth);         // Bottom Left Of The Quad (Right)
 		    GL11.glVertex3f( width,-height,-depth);         // Bottom Right Of The Quad (Right)
+			GL11.glColor3f(1.0f, 1.0f, 1.0f);
 		GL11.glEnd();
 		GL11.glTranslatef(-pos.x(), -pos.y(), -pos.z());
-		GL11.glColor3f(1.0f, 1.0f, 1.0f);
 	}
 	public void act(Environment enviro){
 		applyVelocity();
